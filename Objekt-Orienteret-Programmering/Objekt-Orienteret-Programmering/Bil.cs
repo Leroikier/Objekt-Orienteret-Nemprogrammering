873380d778e7cb42d1740c47a3e795f675d81c74
Alexander Møller <leroikier@hotmail.com>
2018-08-27 13:25:49 +0200

    <?>

---
+++ Objekt-Orienteret-Programmering/Objekt-Orienteret-Programmering/Bil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objekt_Orienteret_Programmering
+{
+    class Bil
+    {
+
+
+    }
+}
